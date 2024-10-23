f488360d9b9c0ba700303b9c233077626db25880
Jéssica Silva <jessica.akyla@gmail.com>
2024-10-23 16:06:19 -0300

    1006 - Média 2

---
+++ Python/1006.py
@@ -0,0 +1,9 @@
+# Média 2
+a = float(input())
+b = float(input())
+c = float(input())
+peso_a = 2 * a
+peso_b = 3 * b
+peso_c = 5 * c
+soma = (peso_a + peso_b + peso_c) / 10
+print(f"MEDIA = {soma:.1f}")
