5f5fa5e92e34492488837f6fc2270cad369b7887
Jéssica Silva <jessica.akyla@gmail.com>
2024-10-24 20:18:20 -0300

    1008 - Salário

---
+++ Python/1008.py
@@ -0,0 +1,8 @@
+# Salário
+numero = int(input())
+horas_trabalhadas = int(input())
+valor_salario = float(input())
+salario = horas_trabalhadas * valor_salario
+
+print(f"NUMBER = {numero}")
+print(f"SALARY = U$ {salario:.2f}")
